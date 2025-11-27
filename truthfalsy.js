a5e4d0781f540098387eed92102ae9d0118aca14
aman22o8 <amankumar.kumar369@gmail.com>
2025-11-27 12:08:47 +0530

    Added faksy examples in local repo

--- truthfalsy.js
+++ truthfalsy.js
@@ -1,1 +1,9 @@
 console.log(0===true)
+console.log(Boolean(false));          // false
+console.log(Boolean(0));              // false
+console.log(Boolean(-0));             // false
+console.log(Boolean(0n));             // false
+console.log(Boolean(""));             // false
+console.log(Boolean(null));           // false
+console.log(Boolean(undefined));      // false
+console.log(Boolean(NaN));            // false
