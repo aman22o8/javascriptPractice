9f9bf2176a25307d400dd4522769df13ff8ca019
AMAN <101987961+aman22o8@users.noreply.github.com>
2025-11-27 12:36:22 +0530

    Truthsy exapmles on rmeote

--- truthfalsy.js
+++ truthfalsy.js
@@ -1,1 +1,10 @@
 console.log(0===true)
+console.log(Boolean(1));              // true
+console.log(Boolean("hello"));        // true
+console.log(Boolean([]));             // true
+console.log(Boolean({}));             // true
+console.log(Boolean("0"));            // true
+console.log(Boolean(-1));             // true
+console.log(Boolean([1, 2]));         // true
+console.log(Boolean(function(){}));   // true
+// Added truthsy examples on Remote
