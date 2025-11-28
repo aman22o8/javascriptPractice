1b16f55e638303b8a35f92c5d1c1c31078163436
aman22o8 <amankumar.kumar369@gmail.com>
2025-11-28 11:40:25 +0530

    alag alag line mein code add krna in local falsy examples

--- truthfalsy.js
+++ truthfalsy.js
@@ -1,5 +1,7 @@
 console.log(0===true)
 
+
+// Added falsey in local 
 console.log(Boolean(false));          // false
 console.log(Boolean(0));              // false
 console.log(Boolean(-0));             // false
@@ -9,6 +11,8 @@ console.log(Boolean(null));           // false
 console.log(Boolean(undefined));      // false
 console.log(Boolean(NaN));            // false
 // Added falsey in local 
+
+// Added truthsy examples on Remote
 console.log(Boolean(1));              // true
 console.log(Boolean("hello"));        // true
 console.log(Boolean([]));             // true
@@ -19,3 +23,17 @@ console.log(Boolean([1, 2]));         // true
 console.log(Boolean(function(){}));   // true
 // Added truthsy examples on Remote
 
+
+
+
+//alag alag line mein code add krna 
+
+console.log(Boolean(false));        // false
+console.log(Boolean(0));            // false
+console.log(Boolean(-0));           // false
+console.log(Boolean(0n));           // false (BigInt zero)
+console.log(Boolean(""));           // false (empty string)
+console.log(Boolean(null));         // false
+console.log(Boolean(undefined));    // false
+console.log(Boolean(NaN));          // false
+//alag alag line mein code add krna 
